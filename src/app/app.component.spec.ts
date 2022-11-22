0c53cd4ad67f8543f80d645e21a3c366ec53e638
SouFien19 <soufienlabiadh19@gmail.com>
2022-11-22 21:38:31 +0100

    update_angular commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sportify-app'`, () => {
+  it(`should have as title 'Sportify-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sportify-app');
+    expect(app.title).toEqual('Sportify-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('sportify-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Sportify-app app is running!');
   });
 });
